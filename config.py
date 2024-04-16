10a54706b9bf7ad340b766d47e188d8e7eacba04
Rdranger9n5 <145197281+Rdranger9n5@users.noreply.github.com>
2024-04-15 21:32:57 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6141105459:AAG2QgssXPV2at540w1CextAZJIrSRH8Kq4")
+API_ID = int(os.environ.get("API_ID", "3566507"))
+API_HASH = os.environ.get("API_HASH", "194e4ba8b6787e11274f47ae63eb56ba")
+OWNER = int(os.environ.get("OWNER", "1654009706"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "RenameSfbot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Vipmoviez")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001720888155"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://renamesfbot:renamesfbot@renamesfbot.vvjumza.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "renamesfbot")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
