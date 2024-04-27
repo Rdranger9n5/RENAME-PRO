afd76ef0d5abee8cd358ff6211c0ae084ce48395
Rdranger9n5 <145197281+Rdranger9n5@users.noreply.github.com>
2024-04-26 22:00:20 -0700

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,16 @@ API_HASH = os.environ.get("API_HASH", "194e4ba8b6787e11274f47ae63eb56ba")
 OWNER = int(os.environ.get("OWNER", "1654009706"))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "RenameSfbot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "Vipmoviez")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "VipMoviez")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001720888155"))
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://renamesfbot:renamesfbot@renamesfbot.vvjumza.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "renamesfbot")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Rolexfilter:Rolexfilter@rolex.0o0saa0.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
+
+
 
 
 
